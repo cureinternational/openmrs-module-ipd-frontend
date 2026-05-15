e3136b2db5d5f6b9d2949691ec872e65cd1ba2fa
avnitanwar <avnitanwar.at1024@gmail.com>
2026-05-15 16:26:41 +0530

    Avni |HIVE-111652| Added loading dose check for ipd dashboard

--- src/utils/CommonUtils.jsx
+++ src/utils/CommonUtils.jsx
@@ -287,6 +287,7 @@ export const mockConfig = {
   medicationTags: {
     asNeeded: "Rx-PRN",
     "STAT (Immediately)": "Rx-STAT",
+    "Loading Dose": "Rx-STAT",
     default: "Rx",
     emergency: "EMERG",
   },
@@ -391,6 +392,7 @@ export const mockConfigFor12HourFormat = {
   medicationTags: {
     asNeeded: "Rx-PRN",
     "STAT (Immediately)": "Rx-STAT",
+    "Loading Dose": "Rx-STAT",
     default: "Rx",
     emergency: "EMERG",
   },
